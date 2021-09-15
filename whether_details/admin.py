4f783dff5cec7ea0246aa08820dc9796f34e8c36
Vinod kumar uttaravalli <uttaravalli.vinod@gmail.com>
2021-09-16 00:38:37 +0530

    created Model called Whether for storing required info and also added that model into admin page.

--- whether_details/admin.py
+++ whether_details/admin.py
@@ -1,3 +1,8 @@
+from django.contrib.gis.admin import OSMGeoAdmin
 from django.contrib import admin
+from .models import Whether
 
 # Register your models here.
+@admin.register(Whether)
+class WhetherAdmin(OSMGeoAdmin):
+    list_display=['location','date','city']
--- whether_details/models.py
+++ whether_details/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class Whether(models.Model):
+    location=models.PointField()
+    date=models.DateTimeField(auto_now_add=True)
+    city=models.CharField(max_length=100,default="Not Updated")
+
